ad307904566ca035b6fc42b5077f82f603c31110
陈威彪 <cwb2@meitu.com>
2020-04-10 14:31:21 +0800

    【feature】使用ebo绘制三角形

--- LearnOpenGL/triangle_vbo/main.cpp
+++ LearnOpenGL/triangle_vbo/main.cpp
@@ -50,7 +50,10 @@ int main(int argc, const char * argv[]) {
         0.5f, -0.5f, 0.0f, // right
         0.0f,  0.5f, 0.0f  // top
     };
-    GLuint vbo, vao;
+    unsigned short indices[] = {
+        0, 1, 2
+    };
+    GLuint vbo, vao, ebo;
     // mac 4.0 以后需要绑定vao
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
@@ -61,7 +64,10 @@ int main(int argc, const char * argv[]) {
     glEnableVertexAttribArray(programLocation);
     glVertexAttribPointer(programLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glUseProgram(shader.getProgram());
-
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(vao);
     // render loop
@@ -69,10 +75,10 @@ int main(int argc, const char * argv[]) {
     {
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        GLERROR_CHECK();
         glUseProgram(shader.getProgram());
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_SHORT, 0);
         glBindVertexArray(0);
         glUseProgram(0);
         // input
